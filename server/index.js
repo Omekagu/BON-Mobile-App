114fc4d43674867b3a593753c2d0e6093c0d9fce
Omekagu Chukwuebuka <mikecheq5@gmail.com>
2025-05-30 11:54:20 +0100

    fix: Update CORS middleware to allow requests from any origin

--- server/index.js
+++ server/index.js
@@ -28,8 +28,11 @@ const server = http.createServer(app)
 
 // Middleware
 dotenv.config()
-app.use(cors())
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }))
+app.use(
+  cors({
+    origin: '*'
+  })
+)
 app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ limit: '50mb', extended: true }))
 
